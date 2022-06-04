0ca5cb341f71d4829651b6543d5bc4e76102aa3b
Paolo Cisneros <fercis2011@hotmail.com>
2022-06-04 18:20:09 -0500

    Street connected update

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from pyvis.network import *
 
 list_intersections = []
 
+nodes = []
+
 # Read intersections
 with open("Lima-intersecciones.csv", "r", encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     cont = 0
     for line in csv_reader:
-        if cont > 27:
+        if cont > 200:
             break
         else:
             intersection = Intersection(line[1], line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10],line[11],line[12],line[13],line[14])
             list_intersections.append(intersection)
+            nodes.append(line[5])
             cont += 1
 
 # Now create list adjacency
@@ -29,14 +32,11 @@ for i in list_intersections:
         listAdjacency.append(list)
 
 # Now create graph
-graph = Network("500px", "500px")
+graph = Network("1000px", "1000px")
 
 # Add vertices - node
-for i in listAdjacency:
-    graph.add_node(i[0], label = i[0])
-
-graph.add_node("20265", label = "20265")
-print(listAdjacency)
+for i in nodes:
+    graph.add_node(i, label = i)
 # Add edges
 for i in listAdjacency:
     graph.add_edge(i[0], i[1], weight =.87)
