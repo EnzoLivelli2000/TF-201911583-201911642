46d5b35e86691aaddff71a07fd73b441c781d1d5
Paolo Cisneros <fercis2011@hotmail.com>
2022-06-04 18:03:27 -0500

    Street connected

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ with open("Lima-intersecciones.csv", "r", encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     cont = 0
     for line in csv_reader:
-        if cont > 11:
+        if cont > 27:
             break
         else:
             intersection = Intersection(line[1], line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10],line[11],line[12],line[13],line[14])
@@ -26,7 +26,7 @@ for i in list_intersections:
             list.append(i.getidBeginInter())
             list.append(j.getidBeginInter())
     if len(list) > 1:
-        listAdjacency.append(tuple(list))
+        listAdjacency.append(list)
 
 # Now create graph
 graph = Network("500px", "500px")
@@ -35,8 +35,10 @@ graph = Network("500px", "500px")
 for i in listAdjacency:
     graph.add_node(i[0], label = i[0])
 
+graph.add_node("20265", label = "20265")
+print(listAdjacency)
 # Add edges
 for i in listAdjacency:
     graph.add_edge(i[0], i[1], weight =.87)
 
-graph.show("lala.html")
+graph.show("lima.html")
