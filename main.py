dfa1625ea34303d1221c536c9d9b2810c006d2e7
Paolo Cisneros <fercis2011@hotmail.com>
2022-06-04 20:26:54 -0500

    Interfaz added

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with open("Lima-intersecciones.csv", "r", encoding="utf8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     cont = 0
     for line in csv_reader:
-        if cont > 200:
+        if cont > 50:
             break
         else:
             intersection = Intersection(line[1], line[2],line[3],line[4],line[5],line[6],line[7],line[8],line[9],line[10],line[11],line[12],line[13],line[14])
@@ -40,5 +40,6 @@ for i in nodes:
 # Add edges
 for i in listAdjacency:
     graph.add_edge(i[0], i[1], weight =.87)
-
+# Draw graph
+graph.show_buttons(filter_=["physics"])
 graph.show("lima.html")
